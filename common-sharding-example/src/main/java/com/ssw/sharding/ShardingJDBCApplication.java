fbc1f0eb1d0c8afb1cefb34770a5604bd96a03ad
aixiaoqie <2390734879@qq.com>
2020-12-03 22:59:08 +0800

    更新pom文件,搭建脚手架

---
+++ common-sharding-example/src/main/java/com/ssw/sharding/ShardingJDBCApplication.java
@@ -0,0 +1,11 @@
+package com.ssw.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJDBCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJDBCApplication.class,args);
+    }
+}
